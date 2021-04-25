63fb0ddab9dc9d6ba9976c4606b4aac9f8b6f2d5
haru123-1 <haru123-123@users.noreply.github.com>
2021-04-25 10:06:09 +0900

    修正

--- files/js/main.js
+++ files/js/main.js
@@ -2,51 +2,67 @@ $(function () {
 
   const $win = $(window);
 
-  (function() {
-	  const $btnMenu = $('.btn-menu');
-	  const $nav = $('header > nav');
-	  const isAnimate = 'is-animate';
-	  const isOpen = 'is-open';
+  (function () {
+    const $btnMenu = $('.btn-menu');
+    const $nav = $('header > nav');
+    const isAnimate = 'is-animate';
+    const isOpen = 'is-open';
 
-  $btnMenu.on('click', function () {
-    const $this = $(this);
-    if ($nav.is(':animated')) {
-      return;
-    }
-    if (!$this.hasClass(isOpen)) {
-      $nav.slideDown();
-      $this.addClass(isOpen).addClass(isAnimate);
-    } else {
-      $nav.slideUp();
-      $this.removeClass(isOpen).removeClass(isAnimate);
-    }
-  });
-
-  $win.on('resize', function () {
-    if ($win.width() > 768) {
-      $nav.show();
-      $btnMenu.removeClass(isOpen).removeClass(isAnimate);
-    } else {
-      if (!$btnMenu.hasClass(isOpen)) {
-        $nav.hide();
+    $btnMenu.on('click', function () {
+      const $this = $(this);
+      if ($nav.is(':animated')) {
+        return;
       }
-    }
-  });();
+      if (!$this.hasClass(isOpen)) {
+        $nav.slideDown();
+        $this.addClass(isOpen).addClass(isAnimate);
+      } else {
+        $nav.slideUp();
+        $this.removeClass(isOpen).removeClass(isAnimate);
+      }
+    });
 
-  (function() {
-  	$win.on('scroll', function() {
-  		const $heading = $('.mask_inner');
-  		const isAnimate = 'is-animate';
-  		$heading.each(function () {
-  			const $this = $(this);
-  			const headingTop = $this.offset().top;
-  			const scrollPos = $win.scrollTop();
-  			const wh = $win.height();
-  			id (scrollPos > headingTop - wh + (wh / 4)) {
-  				$this.addClass(isAnimate);
-  			}
-  		});
-  	});
+    $win.on('resize', function () {
+      if ($win.width() > 768) {
+        $nav.show();
+        $btnMenu.removeClass(isOpen).removeClass(isAnimate);
+      } else {
+        if (!$btnMenu.hasClass(isOpen)) {
+          $nav.hide();
+        }
+      }
+    });
   })();
 
+  (function () {
+    $win.on('scroll', function () {
+      const $heading = $('.mask_inner');
+      const isAnimate = 'is-animate';
+      $heading.each(function () {
+        const $this = $(this);
+        const headingTop = $this.offset().top;
+        const scrollPos = $win.scrollTop();
+        const wh = $win.height();
+        if (scrollPos > headingTop - wh + (wh / 4)) {
+          $this.addClass(isAnimate);
+        }
+      });
+    });
+  })();
+
+  (function () {
+
+    const $body = $('body, html');
+    const duration = 1000;
+    const motion = 'swing';
+
+    $('a[href^="#"]').on('click', function () {
+      const $this = $(this);
+      const href = $this.attr('href');
+      const target = href === '#top' ? 'body' : href;
+      const position = $(target).offset().top;
+      $body.animate({ scrollTop: position }, duration, motion);
+    });
+
+  })();
 });
